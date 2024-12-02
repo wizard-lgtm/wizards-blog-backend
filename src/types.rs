8bcfe33f87d786c698c6bfca82f1a6bd232d6ccf
wizard-lgtm <rippiten23@gmail.com>
2024-12-02 06:12:30 +0300

    Content is optional rn

--- src/types.rs
+++ src/types.rs
@@ -15,7 +15,7 @@ pub enum Tags {
 pub struct Note {
     pub(crate) id: Uuid,              // UUID for unique identification
     pub(crate) title: String,         // Title of the note
-    pub(crate) content: String,       // Markdown text
+    pub(crate) content: Option<String>,       // Markdown text
     pub(crate) created_at: u64,       // Unix epoch time
     pub(crate) updated_at: u64,       // Unix epoch time
     pub(crate) tags: Vec<Tags>,       // Array of tags
